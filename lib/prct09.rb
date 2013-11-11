daa68bea8c62adc603d6f8fe46796bab088f18cf
alu0100700459 <alu0100700459@ull.edu.es>
2013-11-11 12:10:07 -0500

    Añadidos los ficheros de implementación de las clases.

--- lib/prct09.rb
+++ lib/prct09.rb
@@ -1,4 +1,8 @@
 require "prct09/version"
+require "prct09/fraccion"
+require "prct09/matriz"
+require "prct09/matriz_densa"
+require "prct09/matriz_dispersa"
 
 module Prct09
   # Your code goes here...
