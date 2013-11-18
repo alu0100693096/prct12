8da1c41a36e3df4e16d92813d2a5214718ce172f
alu0100700459 <alu0100700459@ull.edu.es>
2013-11-18 13:46:38 -0500

    Añadido esqueleto de las pruebas para la matriz dispersa

---
+++ spec/matrizdispersa_spec.rb
@@ -0,0 +1,40 @@
+require "prct09"
+
+describe Prct09::MatrizDispersa do
+  before :all do
+    # definir las matrices de enteros y fracciones
+  end
+
+  describe "Operaciones con enteros" do
+    before :all do
+      # Crear matrices de enteros
+    end
+
+    it "Debe..." do
+      # ...
+    end
+    # ...
+  end
+
+  describe "Operaciones con fracciones" do
+    before :all do
+    	# Crear matrices de fracciones
+    end
+    
+    it "Debe..." do
+      # ...
+    end
+    # ...
+  end
+
+  describe "Operaciones combinadas" do
+    before :all do
+    	# Crear matrices de fracciones y enteros
+    end
+    
+    it "Debe..." do
+      # ...
+    end
+    # ...
+  end
+end
